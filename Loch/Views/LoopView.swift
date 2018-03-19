393e22cdc7ba5dbf4ded0f418b3cf0d8f719712b
Eric T Cormack <bobryuu@gmail.com>
2018-03-18 19:33:12 -0500

    feat(LoopView): Add bevel to track with the help of some vector arithmatic

--- Loch/Views/LoopView.swift
+++ Loch/Views/LoopView.swift
@@ -10,11 +10,26 @@ import UIKit
 
 @IBDesignable
 class LoopView: UIView {
-    let trackColor = UIColor.black
-    let loopScale: CGFloat = 0.75
-    let lineWidth: CGFloat = 3
+    var trackColor: UIColor = UIColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 1)
+    var trackShade: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+    var trackLight: UIColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+    
+    var loopScale: CGFloat = 0.75
+    var lineWidth: CGFloat = 3
     
     override func draw(_ rect: CGRect) {
+        let downRect = CGRect(origin: rect.origin + (0.0, 1.0), size: rect.size)
+        let downPath = UIBezierPath(ovalIn: makeLoopRect(for: downRect))
+        downPath.lineWidth = lineWidth
+        trackLight.setStroke()
+        downPath.stroke()
+        
+        let upRect = CGRect(origin: rect.origin - (0.0, 1.0), size: rect.size)
+        let upPath = UIBezierPath(ovalIn: makeLoopRect(for: upRect))
+        upPath.lineWidth = lineWidth
+        trackShade.setStroke()
+        upPath.stroke()
+        
         let path = UIBezierPath(ovalIn: makeLoopRect(for: rect))
         path.lineWidth = lineWidth
         trackColor.setStroke()
@@ -26,6 +41,6 @@ class LoopView: UIView {
         let size = CGSize(width: dimension, height: dimension)
         let offset = CGPoint(x: (rect.width - dimension) / 2, y: (rect.height - dimension) / 2)
         
-        return CGRect(origin: offset, size: size)
+        return CGRect(origin: offset + rect.origin, size: size)
     }
 }
