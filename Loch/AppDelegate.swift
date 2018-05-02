91c2c35dab9e2a7f4918d0453aa44653543e3af6
Eric T Cormack <bobryuu@gmail.com>
2018-05-01 20:34:48 -0500

    Beginning calls to Notification Center

--- Loch/AppDelegate.swift
+++ Loch/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { if let error = $1 { print(error) } }
+        
         return true
     }
 
